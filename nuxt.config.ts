dbf5db78da66afda4d222b827b67413ffce1a3c3
Willi Loayza <willy200522@gmail.com>
2025-12-16 21:22:05 -0500

    init config w/ tailwind

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',  
-    modules: ['@nuxt/ui'],
-      css: ['~/assets/css/main.css'],
+  compatibilityDate: "2025-07-15",
+  modules: ["@nuxt/ui"],
+  css: ["./app/assets/css/main.css"],
 
-  devtools: { enabled: false }, 
-
-})
+  devtools: { enabled: false },
+});
