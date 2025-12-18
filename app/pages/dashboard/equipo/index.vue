7b0942a348aee828d71575b802f973581acf0008
Willi <willy200522@gmail.com>
2025-12-18 16:38:47 -0500

    add db

---
+++ app/pages/dashboard/equipo/index.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+definePageMeta({
+  layout: "dashboard-layout", // Debe coincidir con el nombre del archivo en la carpeta layouts
+});
+const UButton = resolveComponent("UButton");
+const UCheckbox = resolveComponent("UCheckbox");
+const UBadge = resolveComponent("UBadge");
+const UDropdownMenu = resolveComponent("UDropdownMenu");
+
+const UInput = resolveComponent("UInput");
+</script>
+
+<template>
+  <div class="flex-1 divide-y divide-accented w-full">
+    <div class="flex items-center gap-2 px-4 py-3.5 overflow-x-auto">
+      <UInput placeholder="Filter emails..." />
+
+      <UDropdownMenu>
+        <UButton
+          label="Columns"
+          color="neutral"
+          variant="outline"
+          trailing-icon="i-lucide-chevron-down"
+          class="ml-auto"
+          aria-label="Columns select dropdown"
+        />
+      </UDropdownMenu>
+    </div>
+
+    <UTable>
+      <template #expanded="{ row }">
+        <pre>{{ row.original }}</pre>
+      </template>
+    </UTable>
+
+    <div class="px-4 py-3.5 text-sm text-muted"></div>
+  </div>
+</template>
