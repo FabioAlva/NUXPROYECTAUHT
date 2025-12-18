9bfb615364cd4eceab9f0a335b5eb8e5b543c5b4
Willi <willy200522@gmail.com>
2025-12-18 17:06:21 -0500

    add logout function

--- app/components/Header.vue
+++ app/components/Header.vue
@@ -1,31 +1,37 @@
 <script setup lang="ts">
-
 const props = defineProps<{
-  title?: string
-  toggleSidebar: () => void
-}>()
+  title?: string;
+  toggleSidebar: () => void;
+}>();
 
-
-
+const { signOut } = useAuth();
 </script>
 
 <template>
-  <div  class="grid grid-cols-[250px_1fr_250px] h-[60px] bg-[var(--color-venice-blue-800)] px-4 items-center shadow-xl z-20 border-none">
-
-    <div class=" col-start-1 col-end-1    w-full h-full flex justify-center items-center gap-1">
-         <Logo />
-      <UButton  @click="props.toggleSidebar"   icon="i-heroicons-bars-3" class="bg-transparent w-[40px] h-[40px] items-center justify-center"/>
-      </div>
-    <div class="col-start-2 col-end-2   "> </div>
-
-    <div class="col-start-3 col-end-3">
-    <UButton                icon="i-lucide-log-out" label="Cerrar sessión" :ui="{ base: 'bg-transparent hover:bg-blue-200' }"/>
+  <div
+    class="grid grid-cols-[250px_1fr_250px] h-[60px] bg-[var(--color-venice-blue-800)] px-4 items-center shadow-xl z-20 border-none"
+  >
+    <div
+      class="col-start-1 col-end-1 w-full h-full flex justify-center items-center gap-1"
+    >
+      <Logo />
+      <UButton
+        @click="props.toggleSidebar"
+        icon="i-heroicons-bars-3"
+        class="bg-transparent w-[40px] h-[40px] items-center justify-center"
+      />
     </div>
-  
-  
+
+    <div class="col-start-2 col-end-2"></div>
+
+    <div class="col-start-4 col-end-4">
+      <UButton
+        icon="i-lucide-log-out"
+        label="Cerrar sessión"
+        :ui="{ base: 'bg-transparent hover:bg-[var(--color-venice-blue-600)]' }"
+        class="cursor-pointer"
+        @click="signOut({ redirectTo: '/' })"
+      />
+    </div>
   </div>
-
-
-  
-
 </template>
--- app/components/SiderMenu.vue
+++ app/components/SiderMenu.vue
@@ -1,90 +1,92 @@
 <script setup lang="ts">
-import type { NavigationMenuItem } from '@nuxt/ui'
-import { boolean } from 'zod';
+import type { NavigationMenuItem } from "@nuxt/ui";
+import { boolean } from "zod";
 
 const items = ref<NavigationMenuItem[][]>([
   [
     {
-      label: 'Gestión Principal',
-      type: 'label'
+      label: "Gestión Principal",
+      type: "label",
     },
     {
-      label: 'Proyectos',
-      icon: 'i-lucide-folder-kanban',
+      label: "Proyectos",
+      icon: "i-lucide-folder-kanban",
       children: [
         {
-          label: 'Vista General',
-          description: 'Estado global de todos los proyectos activos.',
-          icon: 'i-lucide-layout-dashboard',
-          to: '/dashboard/proyectos'
+          label: "Vista General",
+          description: "Estado global de todos los proyectos activos.",
+          icon: "i-lucide-layout-dashboard",
+          to: "/dashboard/proyectos",
         },
         {
-          label: 'Cronograma (Gantt)',
-          description: 'Visualiza la línea de tiempo y dependencias.',
-          icon: 'i-lucide-calendar-days',
-          to: '/dashboard/proyectos/gantt'
-        }
-      ]
+          label: "Cronograma (Gantt)",
+          description: "Visualiza la línea de tiempo y dependencias.",
+          icon: "i-lucide-calendar-days",
+          to: "/dashboard/proyectos/gantt",
+        },
+      ],
     },
     {
-      label: 'Equipo y Recursos',
-      icon: 'i-lucide-users',
+      label: "Equipo y Recursos",
+      icon: "i-lucide-users",
       children: [
         {
-          label: 'Miembros',
-          description: 'Asigna roles y permisos a tu equipo.',
-          icon: 'i-lucide-user-plus',
-          to: '/dashboard/equipo'
+          label: "Miembros",
+          description: "Asigna roles y permisos a tu equipo.",
+          icon: "i-lucide-user-plus",
+          to: "/dashboard/equipo",
         },
         {
-          label: 'Carga de Trabajo',
-          description: 'Monitorea la saturación de tareas por persona.',
-          icon: 'i-lucide-bar-chart-big',
-          to: '/dashboard/equipo/carga'
-        }
-      ]
-    }
+          label: "Carga de Trabajo",
+          description: "Monitorea la saturación de tareas por persona.",
+          icon: "i-lucide-bar-chart-big",
+          to: "/dashboard/equipo/carga",
+        },
+      ],
+    },
   ],
   [
     {
-      label: 'Administración',
-      type: 'label'
+      label: "Administración",
+      type: "label",
     },
     {
-      label: 'Reportes',
-      icon: 'i-lucide-file-pie-chart',
-      to: '/dashboard/reportes'
+      label: "Reportes",
+      icon: "i-lucide-file-pie-chart",
+      to: "/dashboard/reportes",
     },
     {
-      label: 'Configuración',
-      icon: 'i-lucide-settings',
-      to: '/dashboard/configuracion'
-    }
-  ]
-])
+      label: "Configuración",
+      icon: "i-lucide-settings",
+      to: "/dashboard/configuracion",
+    },
+  ],
+]);
 
-const props =defineProps({
-  opened : {
+const props = defineProps({
+  opened: {
     type: Boolean,
-    default: true
-  }
-})
-
+    default: true,
+  },
+});
 </script>
 
 <template>
-
-<div 
-  class="h-full bg-[var(--color-venice-blue-800)] flex flex-col transition-all duration-300"
-  :class="props.opened ? 'w-[300px]' : 'w-[70px]'"
->      <UNavigationMenu :collapsed="!props.opened" orientation="vertical" :items="items" class="data-[orientation=vertical]:w-full" :ui="{
-      label: 'text-white hover:text-orange-700',
-      link: 'text-white hover:text-orange-700',
-      linkLeadingIcon: 'text-white',
-      indicator: 'bg-blue-600'
-    }"
-    
+  <div
+    class="h-full bg-[var(--color-venice-blue-800)] flex flex-col transition-all duration-300"
+    :class="props.opened ? 'w-[300px]' : 'w-[70px]'"
+  >
+    <UNavigationMenu
+      :collapsed="!props.opened"
+      orientation="vertical"
+      :items="items"
+      class="data-[orientation=vertical]:w-full"
+      :ui="{
+        label: 'text-white hover:text-orange-700',
+        link: 'text-white hover:text-orange-700',
+        linkLeadingIcon: 'text-white',
+        indicator: 'bg-blue-600',
+      }"
     />
   </div>
 </template>
-
